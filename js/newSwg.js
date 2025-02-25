2768433638524de1feafd6455d9f86c3d7f7dfef
PA-00-bl16 (Galaktionov <pa-00-bl16@schulung-bbw.de>
2025-02-25 14:39:40 +0100

    erste commit

--- js/newSwg.js
+++ js/newSwg.js
@@ -13,28 +13,49 @@ for(let i=0;i<icons_animation.length;i++){
         icons_animation[i].addEventListener("mouseout",function(){
             icons_animation[i].style.transform = "scale(1)";
         });
-    }
-        
+    }        
 }
 /*Kontakt Liste einrichten*/
 
 let kontakt = document.querySelector(".kontakt"),
-    liste = document.querySelector(".kontakt_funktion");
+    liste = document.querySelector(".kontakt_funktion"),
+    closeBtn = document.querySelector(".close");
 
-    kontakt.addEventListener("click", function(){
+    kontakt.addEventListener("mouseover", function(){
         liste.style.display = "block";
     });
 
-/*
     kontakt.addEventListener("mouseout", function(){
         liste.style.display = "none";
+    });  
+
+/*Support window einrichten*/
+
+let support = document.querySelector(".support__button"),
+    support_modal = document.querySelector(".modal__support"),
+    close_support = document.querySelector(".close__support");
+
+    support.addEventListener("click", function(){
+        support_modal.style.display = "block";
     });
 
-    liste.addEventListener("mouseover", function(){
-        liste.style.display = "block";
+    close_support.addEventListener("click", function(){
+        support_modal.style.display = "none";
     });
-*/
 
+    /*Kunden Portal window einrichten*/
+ 
+    let portal = document.querySelector(".portal__button"),
+    kunden_potal_modal = document.querySelector(".kunden_portal"),
+    close_close_kp = document.querySelector(".close__portal");
+
+    portal.addEventListener("click", function(){
+        kunden_potal_modal.style.display = "flex";
+    });
+
+    close_close_kp.addEventListener("click", function(){
+        kunden_potal_modal.style.display = "none";
+    });
 
 
 

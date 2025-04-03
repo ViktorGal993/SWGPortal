e21ea2e33a62f4e1e080b35036e650b2d8b7f52c
Viktor <Viktorkauzg@gmail.com>
2025-04-03 14:12:15 +0200

    php files hinzugefügt

--- index.php
+++ index.php
@@ -253,43 +253,15 @@
         </form>
     </div> 
             <!--PHP -->
-    <?php
+    <?php    
+    
 
     //gebt die Datai
-    //if (!isset($_POST['name']) and !isset($_POST['lname']) and !isset($_POST['email']) ) }?>
-        $name = $_POST['name'];
-        $lname = $_POST['lname'];
-        $email = $_POST['email'];
-        
-        $name = htmlspecialchars($name);
-        $lname = htmlspecialchars($lname);
-        $email = htmlspecialchars($email);
-        
-        # decodierung url
-        $name = urldecode($name);
-        $lname = urldecode($lname);
-        $email = urldecode($email);
-        
-        #delete von probelen
-        $name = trim($name);
-        $lname = trim($lname);
-        $email = trim($email);
-        
-        
-        echo $name, " | ", $lname, " | ", $email;
-        
-        # send of message
-        mail("viktorkauzg@gmail.com", "Brief von Seite", "Name: ".$name. "Nachname:".$lname." . Email: ".$email, "From: example2@mail.ru\r\n");
-        
-        # Prufung
-        if(mail("viktorkauzg@gmail.com", "Brief von Seite", "Name: ".$name. "Nachname:".$lname." . Email: ".$email, "From: example2@mail.ru\r\n"))
-        {
-            echo " die Nachrichr gesendet";
-        }
-        else {
-            echo "Fehler";
-        } 
+    //if (!isset($_POST['name']) and !isset($_POST['lname']) and !isset($_POST['email']) ) }
+       
     ?>
+
+
     <!-- um dem Benutzer eine Rückmeldung über den Status seiner Buchung zu geben-->
     <div id="ruckinfo" class="hidden"></div>
 
--- send.php
+++ send.php
@@ -1,36 +1,27 @@
 <?php
+    include 'mail.php';
 
-$name = $_POST['name'];
-$lname = $_POST['lname'];
-$email = $_POST['email'];
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {    
 
-$name = htmlspecialchars($name);
-$lname = htmlspecialchars($lname);
-$email = htmlspecialchars($email);
-
-# decodierung url
-$name = urldecode($name);
-$lname = urldecode($lname);
-$email = urldecode($email);
-
-#delete von probelen
-$name = trim($name);
-$lname = trim($lname);
-$email = trim($email);
-
-
-echo $name, " | ", $lname, " | ", $email;
-
-# send of message
-mail("viktorkauzg@gmail.com", "Brief von Seite", "Name: ".$name. "Nachname:".$lname." . Email: ".$email, "From: example2@mail.ru\r\n");
-
-# Prufung
-if(mail("viktorkauzg@gmail.com", "Brief von Seite", "Name: ".$name. "Nachname:".$lname." . Email: ".$email, "From: example2@mail.ru\r\n"))
-{
-    echo " die Nachrichr gesendet";
-}
-else {
-    echo "Fehler";
-}
+        $name = trim(htmlspecialchars(urldecode($_POST["name"] ?? "")));    
+        
+        $lname = trim(htmlspecialchars(urldecode($_POST["lname"] ?? "")));    
+        
+        $email = trim(htmlspecialchars(urldecode($_POST["email"] ?? "")));
+         $to = "Viktorkauz@gmail.com"; // Укажите свою почту    
+        
+        $subject = "Brief von Seite";    
+        
+        $message = "Name: $name\nNachname: $lname\nEmail: $email";    
+        
+        $headers = "From: example2@mail.ru\r\nReply-To: $email\r\nContent-Type: text/plain; charset=UTF-8";
+         if (mail($to, $subject, $message, $headers)) {       
+        
+         echo "Die Nachricht gesendet!";    
+        
+        } else {        
+        
+        echo "Fehler!";    }
+    }
 
 ?>
